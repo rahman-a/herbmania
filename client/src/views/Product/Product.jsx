5f55dd106dd5d1c6a61db2d251154a822631f093
rahman-a <ahmpe@lenovo-3.localdomain>
2021-06-16 19:42:41 +0200

    finish adding the the product image, name , mini description,price, quantity and call to action button

--- client/src/views/Product/Product.jsx
+++ client/src/views/Product/Product.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Template from '../../components/Template/Template'
 import {product} from './Product.module.scss'
+import ProductDetails from '../../components/ProductDetails/ProductDetails'
 
 const Product = () => {
     return (
         <Template>
            <div className={product}>
                <div className="container">
-
+                    <ProductDetails/>
                </div>
            </div>
         </Template>
